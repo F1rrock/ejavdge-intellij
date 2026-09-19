ed8a7d7059e99b226a6bef2a52721db79a5a2017
F1rrock <flyrock000@gmail.com>
2026-09-19 22:52:18 +0800

    refactor: use IntellijOut and ResOfSettings in implementation of ActOfAvailableProblems

--- src/main/java/org/ejavdge/action/ActOfAvailableProblems.java
+++ src/main/java/org/ejavdge/action/ActOfAvailableProblems.java
@@ -1,17 +1,13 @@
 package org.ejavdge.action;
 
-import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import org.ejavdge.app.AvailableProblems;
-import org.ejavdge.out.IntellijConsole;
-import org.ejavdge.settings.CredOfSettings;
+import org.ejavdge.out.IntellijOut;
 import org.ejavdge.settings.EjState;
-import org.ejavdge.settings.LocOfSettings;
+import org.ejavdge.settings.ResOfSettings;
 import org.ejavdge.widget.ConsoleWindow;
-import org.ejavdge.workspace.out.WithReport;
-import org.ejavdge.workspace.out.WithoutAnsi;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -23,24 +19,12 @@ public final class ActOfAvailableProblems extends AnAction {
 
     @Override
     public void actionPerformed(final @NotNull AnActionEvent event) {
-        final var console = Objects.requireNonNull(
-                event.getProject()
-            )
-            .getService(ConsoleWindow.class)
-            .console();
         new AvailableProblems(
-            new LocOfSettings(this.state).location(),
-            new CredOfSettings(this.state).credentials(),
-            new WithReport(
-                new WithoutAnsi(
-                    new IntellijConsole(console)
-                ),
-                new WithoutAnsi(
-                    new IntellijConsole(
-                        console,
-                        ConsoleViewContentType.ERROR_OUTPUT
-                    )
-                )
+            new ResOfSettings(this.state).resource(),
+            new IntellijOut(
+                Objects.requireNonNull(event.getProject())
+                    .getService(ConsoleWindow.class)
+                    .console()
             )
         ).run();
     }
